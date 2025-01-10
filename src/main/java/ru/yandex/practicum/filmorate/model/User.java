33b715e299de643156e04b611ce765ccd7ae70f0
Khs <saha20142@yandex.kz>
2025-01-10 16:35:45 -0400

    исправил ошибки по ревью

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,9 +9,10 @@ import java.time.LocalDate;
 @Data
 public class User {
     private Long id;
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email не должен быть пустым")
+    @Email(message = "Неверный формат email. Пожалуйста, укажите действительный адрес электронной почты.")
     private String email;
+    @NotBlank(message = "Логин не должен быть пустым")
     private String login;
     private String name;
     private LocalDate birthday;
