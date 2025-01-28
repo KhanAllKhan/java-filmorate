88cf42e2eaa2e6da7fa973381a1dd3660bbea15a
Khs <saha20142@yandex.kz>
2025-01-28 16:12:53 +0500

    правки по ревью номер 4

--- src/main/java/ru/yandex/practicum/filmorate/Handler/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/Handler/GlobalExceptionHandler.java
@@ -1,12 +1,29 @@
 package ru.yandex.practicum.filmorate.Handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
     @ExceptionHandler(ConditionsNotMetException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -16,14 +33,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
     public ErrorResponse handleNotFoundException(NotFoundException ex) {
         return new ErrorResponse("Не найдено: " + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     public ErrorResponse handleException(Exception ex) {
         return new ErrorResponse("Внутренняя ошибка сервера: " + ex.getMessage());
     }
