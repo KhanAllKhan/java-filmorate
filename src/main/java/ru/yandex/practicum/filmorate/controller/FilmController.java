967410e351dbb9e3f321147c97994535fe43f49e
Khs <saha20142@yandex.kz>
2025-01-28 16:45:38 +0500

    правки по ревью номер 8

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-
 import java.util.List;
 
 @RestController
@@ -16,8 +15,13 @@ public class FilmController {
     private final FilmService filmService;
 
     @GetMapping
-    public List<Film> getAll() {
+    public List<Film> findAll() {
         return filmService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Film findById(@PathVariable Long id) {
+        return filmService.findById(id);
     }
 
     @PostMapping
@@ -26,12 +30,22 @@ public class FilmController {
     }
 
     @PutMapping
-    public Film update(@RequestBody @Valid Film film) {
-        return filmService.update(film);
+    public Film update(@RequestBody @Valid Film newFilm) {
+        return filmService.update(newFilm);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.addLike(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable Long id, @PathVariable Long userId) {
         filmService.removeLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+        return filmService.getPopularFilms(count);
     }
 }
